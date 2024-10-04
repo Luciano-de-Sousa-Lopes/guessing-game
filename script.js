e00690f0861ded88eaa04939924ea2f6c23e1356
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2024-10-04 17:06:02 -0300

    feat: implemented the logic to verify if the user's guess is right or wrong, decrease the score of the and display error or success messages

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+const guessResultElement = document.querySelector('.message');
+const scoreElement = document.querySelector('.score');
+let score = 20;
+scoreElement.textContent = score;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(`${typeof guess}`);
 
   if (!guess) {
     document.querySelector('.error-message').textContent = 'Enter a number';
-  } else {
+  } else if (guess === secretNumber) {
     document.querySelector('.error-message').textContent = '';
+    guessResultElement.innerHTML =
+      '<span class="right-guess">🎉 Correct Number!</span>';
+  } else if (guess > secretNumber) {
+    score--;
+    if (score > 0) {
+      guessResultElement.innerHTML =
+        '<span class="wrong-guess">😞 Number too high!</span>';
+    } else {
+      guessResultElement.innerHTML =
+        '<span class="wrong-guess">😞 You lost the game!</span>';
+    }
+  } else if (guess < secretNumber) {
+    score--;
+    if (score > 0) {
+      guessResultElement.innerHTML =
+        '<span class="wrong-guess">😞 Number too low!</span>';
+    } else {
+      guessResultElement.innerHTML =
+        '<span class="wrong-guess">😞 You lost the game!</span>';
+    }
   }
+  scoreElement.textContent = score;
 });
