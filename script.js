065b69d279a904f6dd7ae9d42f5d241498a361ff
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2024-10-04 12:00:08 -0300

    feat: implemented the logic to get the number from the input field, parse it to a number and check if there is a number and if it is greater than zero

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(`${typeof guess}`);
+
+  if (!guess) {
+    document.querySelector('.error-message').textContent = 'Enter a number';
+  } else {
+    document.querySelector('.error-message').textContent = '';
+  }
+});
