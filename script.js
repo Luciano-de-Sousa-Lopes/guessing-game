1c6cbb2c51eeca94edb0a6ab05b7ebc5fb903d9e
Luciano de Sousa Lopes <lucianodesousalopes.github@gmail.com>
2024-10-05 13:54:07 -0300

    feat: starting to refactor the code

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+function generateSecretNumber() {
+  return Math.trunc(Math.random() * 20) + 1;
+}
 
-const guessResultElement = document.querySelector('.message');
-const scoreElement = document.querySelector('.score');
+function getElement(selector) {
+  return document.querySelector(selector);
+}
+
+function setElementValue(el, property, valueToSet) {
+  if (property === 'textContent') {
+    el.textContent = valueToSet;
+  } else if (property === 'innerHTML') {
+    el.innerHTML = valueToSet;
+  } else {
+    el.value = valueToSet;
+  }
+}
+
+let secretNumber = generateSecretNumber();
+const numberEl = getElement('.number');
+setElementValue(numberEl, 'textContent', secretNumber);
+
+const guessResultEl = getElement('.message');
+
+const scoreEl = getElement('.score');
 let score = 20;
-scoreElement.textContent = score;
+setElementValue(scoreEl, 'textContent', score);
+scoreEl.textContent = score;
 let highScore = 0;
-document.querySelector('.highscore').textContent = highScore;
+
+const highScoreEl = getElement('.highscore');
+setElementValue(highScoreEl, '.textContent', highScore);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,38 +47,66 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('.error-message').textContent = '';
-    guessResultElement.innerHTML =
-      '<span class="right-guess">🎉 Correct Number!</span>';
+
+    setElementValue(
+      guessResultEl,
+      'innerHTML',
+      '<span class="right-guess">🎉 Correct Number!</span>'
+    );
+
     document.querySelector('body').style.backgroundColor = 'cornflowerblue';
     document.querySelector('.number').className = 'number winner';
   } else if (guess > secretNumber) {
     score--;
+
     if (score > 0) {
-      guessResultElement.innerHTML =
-        '<span class="wrong-guess">😞 Number too high!</span>';
+      setElementValue(
+        guessResultEl,
+        'innerHTML',
+        '<span class="wrong-guess">😞 Number too high!</span>'
+      );
     } else {
-      guessResultElement.innerHTML =
-        '<span class="wrong-guess">😞 You lost the game!</span>';
+      setElementValue(
+        guessResultEl,
+        'innerHTML',
+        '<span class="wrong-guess">😞 You lost the game!</span>'
+      );
     }
   } else if (guess < secretNumber) {
     score--;
     if (score > 0) {
-      guessResultElement.innerHTML =
-        '<span class="wrong-guess">😞 Number too low!</span>';
+      setElementValue(
+        guessResultEl,
+        'innerHTML',
+        '<span class="wrong-guess">😞 Number too low!</span>'
+      );
     } else {
-      guessResultElement.innerHTML =
-        '<span class="wrong-guess">😞 You lost the game!</span>';
+      setElementValue(
+        guessResultEl,
+        'innerHTML',
+        '<span class="wrong-guess">😞 You lost the game!</span>'
+      );
     }
   }
-  scoreElement.textContent = score;
+
+  setElementValue(scoreEl, 'textContent', score);
 });
 
+// ON CLICK EVENT RUN THIS CODE
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
+
+  secretNumber = generateSecretNumber();
+
+  scoreEl;
+  setElementValue(scoreEl, 'textContent', score);
+
+  const messageEl = getElement('.message');
+  setElementValue(messageEl, '.textContent', 'Start guessing...');
+
+  const el = getElement('.number');
+  setElementValue(el, 'textContent', '?');
+
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = 'black';
   document.querySelector('.highscore').textContent = highScore;
